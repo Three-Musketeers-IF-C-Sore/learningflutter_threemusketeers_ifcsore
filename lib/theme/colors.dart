6f462f8d0083cb7dab4bf3ec8d50be933b421608
William Antoline <65105895+williamantoline@users.noreply.github.com>
2023-04-08 21:08:52 +0700

    add colors

--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
  
-const Color $primary500 = Color(0x5B50D8); //todo: solve warning
-const Color $secondary500 = Color(0x53B176);
-const Color $neutral500 = Color(0x6D6D6D);
-const Color $black = Color(0x000000);
-const Color $white = Color(0xFFFFFF);
+const Color $primary500 = Color.fromRGBO(91, 80, 216, 1.0);
+const Color $primary300 = Color.fromRGBO(130, 122, 225, 1.0);
+const Color $secondary500 = Color.fromRGBO(83, 177, 118, 1.0);
+const Color $tertiary300 = Color.fromRGBO(161, 161, 161, 1.0);
+const Color $tertiary500 = Color.fromRGBO(109, 109, 109, 1.0);
+const Color $tertiary800 = Color.fromRGBO(59, 59, 59, 1.0);
+const Color $black = Color.fromRGBO(0,0,0,1.0);
+const Color $white = Color.fromRGBO(255,255,255,1.0);
+
